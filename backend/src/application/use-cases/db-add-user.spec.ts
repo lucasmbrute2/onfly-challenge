022a9f36d7802357640d71d92d44b9b903939458
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-27 00:24:22 -0300

    test: ensure that addUserRepository is being called with correct values

---
+++ backend/src/application/use-cases/db-add-user.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it, vi } from 'vitest'
+import { AddUserRepository } from '../protocols'
+import { makeAddUserRepository, makeUserModel } from '../tests/factories'
+import { DbAddUserUseCase } from './db-add-user'
+import { User } from '@/src/domain/entities/user'
+
+interface SutTypes {
+  sut: DbAddUserUseCase
+  addUserRepositoryStub: AddUserRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addUserRepositoryStub = makeAddUserRepository()
+  const dbAddUserUseCase = new DbAddUserUseCase(addUserRepositoryStub)
+
+  return {
+    sut: dbAddUserUseCase,
+    addUserRepositoryStub,
+  }
+}
+
+describe('DbAddUser Use Case', () => {
+  it('Should call AddCompanyRepository with correct values', async () => {
+    const { addUserRepositoryStub, sut } = makeSut()
+    const addSpy = vi.spyOn(addUserRepositoryStub, 'add')
+
+    const response = await sut.add(makeUserModel())
+    const user = new User({ ...makeUserModel(), id: response?.id })
+    expect(addSpy).toHaveBeenCalledWith(user)
+  })
+})
