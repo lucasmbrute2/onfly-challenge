c2433caf5c74df54a958b73351863e68879593fe
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-27 16:47:43 -0300

    fix: import errors

--- backend/src/application/use-cases/db-add-user.ts
+++ backend/src/application/use-cases/db-add-user.ts
@@ -1,24 +1,25 @@
 import { User } from '@/src/domain/entities/user'
 import { AddUserModel, AddUser } from '@/src/domain/use-cases/add-user'
 import { Hasher } from '../protocols/cryptography'
-import { PrismaUserRepository } from '@/src/infra/db/prisma/user-repository'
+import { FindUserByUsernameRepository } from '../protocols/user/find-user-by-username'
+import { AddUserRepository } from '../protocols/user'
 
 export class DbAddUserUseCase implements AddUser {
   constructor(
     private readonly hasher: Hasher,
-    private readonly userRepository: PrismaUserRepository,
+    private readonly addUserRepository: AddUserRepository,
+    private readonly findByUsernameRepository: FindUserByUsernameRepository,
   ) {}
 
   async add(userData: AddUserModel): Promise<User | null> {
-    const userAlreadyExists = await this.userRepository.findByUsername(
-      userData.username,
-    )
+    const userAlreadyExists =
+      await this.findByUsernameRepository.findByUsername(userData.username)
     if (userAlreadyExists) return null
 
     const hashedPassword = await this.hasher.hash(userData?.password)
     const user = new User({ ...userData, password: hashedPassword })
 
-    await this.userRepository.add(user)
+    await this.addUserRepository.add(user)
     return user
   }
 }
