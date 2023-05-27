db5860cffa19614b01bbaa19a1c5c9c01a9e8f77
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-27 13:03:11 -0300

    test: ensure that DbAddUserUseCase returns null if user already exists

--- backend/src/application/use-cases/db-add-user.spec.ts
+++ backend/src/application/use-cases/db-add-user.spec.ts
@@ -1,32 +1,52 @@
 import { describe, expect, it, vi } from 'vitest'
-import { AddUserRepository } from '../protocols/user'
-import { makeAddUserRepository, makeUserModel } from '../tests/factories'
+import { AddUserRepository, FindUserByIdRepository } from '../protocols/user'
+import {
+  makeAddUserRepository,
+  makeFindUserByIdRepository,
+  makeUserModel,
+} from '../tests/factories'
 import { DbAddUserUseCase } from './db-add-user'
 import { User } from '@/src/domain/entities/user'
+import { makeUser } from '@/src/domain/entities/tests/factories'
 
 interface SutTypes {
   sut: DbAddUserUseCase
   addUserRepositoryStub: AddUserRepository
+  findUserByIdRepository: FindUserByIdRepository
 }
 
 const makeSut = (): SutTypes => {
   const addUserRepositoryStub = makeAddUserRepository()
-  const dbAddUserUseCase = new DbAddUserUseCase(addUserRepositoryStub)
+  const findUserByIdRepository = makeFindUserByIdRepository()
+  const dbAddUserUseCase = new DbAddUserUseCase(
+    addUserRepositoryStub,
+    findUserByIdRepository,
+  )
 
   return {
     sut: dbAddUserUseCase,
     addUserRepositoryStub,
+    findUserByIdRepository,
   }
 }
 
 describe('DbAddUser Use Case', () => {
-  it('Should call AddCompanyRepository with correct values', async () => {
+  it('Should call AddUserRepository with correct values', async () => {
     const { addUserRepositoryStub, sut } = makeSut()
     const addSpy = vi.spyOn(addUserRepositoryStub, 'add')
 
     const response = await sut.add(makeUserModel())
     const user = new User({ ...makeUserModel(), id: response?.id })
     expect(addSpy).toHaveBeenCalledWith(user)
+  })
+
+  it('Should return null if FindUserByIdRepository reach an user', async () => {
+    const { sut, findUserByIdRepository } = makeSut()
+    vi.spyOn(findUserByIdRepository, 'find').mockReturnValueOnce(
+      Promise.resolve(makeUser()),
+    )
+    const response = await sut.add(makeUserModel())
+    expect(response).toBe(null)
   })
 
   it('Should return an User on success', async () => {
